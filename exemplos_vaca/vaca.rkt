0d5934cc1c45c57a29321e89f5012f9d6adf8dc5
Helio <heliohenrique3@gmail.com>
2022-03-09 14:31:08 -0300

    Aula 08/03 -- termino primeira versao do jogo da vaca

--- exemplos_vaca/vaca.rkt
+++ exemplos_vaca/vaca.rkt
@@ -15,11 +15,13 @@
 
 (define MEIO (/ LARGURA-CENARIO 2))
 
-(define LIMITE-ESQUERDO 0)
-(define LIMITE-DIREITO LARGURA-CENARIO)
+(define IMG-VACA (bitmap "vaca.png"))
+
+(define LIMITE-ESQUERDO (+ 0 (/ (image-width IMG-VACA) 2)))
+(define LIMITE-DIREITO (- LARGURA-CENARIO (/ (image-width IMG-VACA) 2)))
 
 (define Y (/ ALTURA-CENARIO 2))
-(define IMG-VACA (bitmap "vaca.png"))
+
 
 (define DX-PADRAO 3)
 (define TC-VIRA " ")
@@ -29,7 +31,8 @@
 
 (define-struct vaca (x dx))
 ;; Vaca é (make-vaca Natural Inteiro)
-;; interp. posicao da vaca no eixo x e a direção dx, em pixels
+;; interp. posicao da vaca no eixo x e a direção dx, em pixels.
+;; se dx>=0, vaca estará apontando para a direita, se dx<0, estará apontando p/ esquerda
 
 ;; Exemplos:
 (define VACA-INICIO (make-vaca LIMITE-ESQUERDO DX-PADRAO))
@@ -47,52 +50,102 @@
 ;; Funções:
 
 
-;; EstadoMundo -> EstadoMundo
-;; produz o próximo ...
-;; !!!
-#;
-(define (tock estado) ...)
+;; Vaca -> Vaca
+;; produz o próximo estado da vaca
+
+(define (atualiza-vaca v)
+  (cond
+
+    [(> (+ (vaca-x v) (vaca-dx v)) LIMITE-DIREITO)
+     (make-vaca LIMITE-DIREITO (- (vaca-dx v)))]
+
+    [(< (+ (vaca-x v) (vaca-dx v)) LIMITE-ESQUERDO)
+     (make-vaca LIMITE-ESQUERDO (- (vaca-dx v)))]
+    
+    [else
+     (make-vaca (+ (vaca-x v) (vaca-dx v))  ;novo x
+             (vaca-dx v)   ;novo dx
+             )
+     ]
+    )
+  )    
+
+;; Testes
+; CENARIOS DA VACA ANDANDO DE BOA PARA FRENTE
+(check-expect (atualiza-vaca (make-vaca LIMITE-ESQUERDO 3))  ; CHAMADA
+              (make-vaca (+ LIMITE-ESQUERDO 3) 3))    ; RESULTADO ESPERADO
+(check-expect (atualiza-vaca (make-vaca MEIO 3))  ; CHAMADA
+              (make-vaca (+ MEIO 3) 3))  
+
+;; CENARIOS DA VACA BATENDO NA CERCA DA DIREITO
+(check-expect (atualiza-vaca (make-vaca LIMITE-DIREITO 3))
+              (make-vaca LIMITE-DIREITO -3))
+(check-expect (atualiza-vaca (make-vaca (- LIMITE-DIREITO 1) 3))
+              (make-vaca LIMITE-DIREITO -3))
+(check-expect (atualiza-vaca (make-vaca (- LIMITE-DIREITO 2) 3))
+              (make-vaca LIMITE-DIREITO -3))
+
+;; CENARIOS DA VACA ANDANDO DE BOA PARA TRAS
+(check-expect (atualiza-vaca (make-vaca MEIO -3)) 
+              (make-vaca (- MEIO 3) -3))
+(check-expect (atualiza-vaca (make-vaca (+ LIMITE-ESQUERDO 3) -3)) 
+              (make-vaca LIMITE-ESQUERDO -3))
+
+;; CENARIOS DA VACA BATENDO NA CERCA DA ESQUERDA
+(check-expect (atualiza-vaca (make-vaca LIMITE-ESQUERDO -3))
+              (make-vaca LIMITE-ESQUERDO 3))
+(check-expect (atualiza-vaca (make-vaca (+ LIMITE-ESQUERDO 1) -3))
+              (make-vaca LIMITE-ESQUERDO 3))
+(check-expect (atualiza-vaca (make-vaca (+ LIMITE-ESQUERDO 2) -3))
+              (make-vaca LIMITE-ESQUERDO 3))   
 
 
-;; EstadoMundo -> Image
-;; desenha 
-;; !!!
-#;
-(define (desenha-mundo estado) ...)
+;; Vaca -> Image
+;; desenha a vaca no pasto
+
+(define (desenha-vaca v)
+  (cond
+    [(< (vaca-dx v) 0)
+     (place-image (flip-horizontal IMG-VACA) (vaca-x v) Y CENARIO)]
+    [else
+     (place-image IMG-VACA (vaca-x v) Y CENARIO)]
+    )
+  )
+
+; Testes
+(check-expect (desenha-vaca (make-vaca 0 3))
+              (place-image IMG-VACA 0 Y CENARIO))
+  
+(check-expect (desenha-vaca (make-vaca MEIO 3))
+              (place-image IMG-VACA MEIO Y CENARIO))
+(check-expect (desenha-vaca (make-vaca MEIO -3))
+              (place-image (flip-horizontal IMG-VACA) MEIO Y CENARIO))
 
 
-;; EstadoMundo KeyEvent -> EstadoMundo
-;; quando teclar ...  produz ...  <apagar caso não precise usar>
-#;
-(define (handle-key estado ke)
-  (cond [(key=? ke " ") (... estado)]
-        [else
-         (... estado)]))
+;; Vaca KeyEvent -> Vaca
+;; quando teclar ESPAÇO vira a vaca (inverte dx)
+; !!! (TODO)
 
-;; EstadoMundo Integer Integer MouseEvent -> EstadoMundo
-;; Quando fazer ... nas posições x y no mouse produz ...   <apagar caso não precise usar>
-#;
-(define (handle-mouse estado x y me)
-(cond [(mouse=? me "button-down") (... estado x y)]
-      [else
-       (... estado x y)]))
+(define (trata-tecla v ke)
+  (cond [(key=? ke " ") (make-vaca (vaca-x v) (- (vaca-dx v)))]
+        [else v])
+        )
 
 
+;; Testes
+(check-expect (trata-tecla (make-vaca MEIO 3) " ")
+              (make-vaca MEIO -3))
+(check-expect (trata-tecla (make-vaca MEIO 3) "a")
+              (make-vaca MEIO 3))
 
-
-;; EstadoMundo -> EstadoMundo
-;; inicie o mundo com ...
+;; Vaca -> Vaca
+;; inicie o mundo com (main VACA-INICIO)
 ;; 
-#;
-(define (main estado)
-  (big-bang estado               ; EstadoMundo   (estado inicial do mundo)
-            (on-tick   tock)     ; EstadoMundo -> EstadoMundo    
+(define (main v)
+  (big-bang v               ; Vaca   (estado inicial do mundo)
+            (on-tick   atualiza-vaca)     ; Vaca -> Vaca    
                                    ;(retorna um novo estado do mundo dado o atual a cada tick do clock)
-            (to-draw   desenha-mundo)   ; EstadoMundo -> Image   
+            (to-draw   desenha-vaca)   ; Vaca -> Image   
                                           ;(retorna uma imagem que representa o estado atual do mundo)
-            (stop-when ...)      ; EstadoMundo -> Boolean    
-                                    ;(retorna true se o programa deve terminar e false se deve continuar)
-            (on-mouse  ...)      ; EstadoMundo Integer Integer MouseEvent -> EstadoMundo    
-                                    ;(retorna um novo estado do mundo dado o estado atual e uma interação com o mouse)
-            (on-key    ...)))    ; EstadoMundo KeyEvent -> EstadoMundo
+            (on-key    trata-tecla)))    ; Vaca KeyEvent -> Vaca
                                     ;(retorna um novo estado do mundo dado o estado atual e uma interação com o teclado)
